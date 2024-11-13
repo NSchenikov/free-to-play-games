d63ede318f262c8f390f391b04df22e4f7d76495
Nikita-Schenikov <nikita-shenikov@mail.ru>
2024-11-14 00:58:51 +0300

    no success

--- src/features/server.js
+++ src/features/server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
 const app = express();
 const PORT = 8080;
-
-
 app.use((req, res, next) => {
     console.log(`Received request for: ${req.url}`);
     next();
 });
-
-
 app.use('/api', createProxyMiddleware({
     target: 'https://www.freetogame.com',
     changeOrigin: true,
@@ -18,10 +13,7 @@ app.use('/api', createProxyMiddleware({
         '^/api': '/api',
     },
 }));
-
-
 app.use('/assets', express.static('public/assets')); 
-
 app.listen(PORT, () => {
     console.log(`Proxy server is running at http://localhost:${PORT}`);
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,15 +4,13 @@ import apiSlice from '../features/api/apiSlice';
 
 const store = configureStore({
   reducer: {
-    // Добавьте редюсер API
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware), // Добавьте middleware API
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
 
-// Типизация для RootState и AppDispatch (для использования с useSelector и useDispatch, если потребуется)
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
